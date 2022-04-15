c97efe61066b6c01a80fc583279733cee54ac870
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-15 23:49:40 +0430

    محاسبه گر میانگین با استفاده از تعریف تابع

---
+++ 34- def introduction ver16.py
@@ -0,0 +1,22 @@
+def import_os():
+    import os
+    os.system('cls')
+import_os()
+
+def list_builder():
+    user_input = input('Write your word:')
+    list_words = []
+    while user_input != 'finish':     
+        list_words.append(user_input)
+        user_input = input('Write your word:')
+    return list_words
+    
+def average_calculator(list_exp):
+    all_words = 0
+    for word in list_exp:
+        all_words += len(word)
+    return all_words / len(list_exp)
+
+user_input_words = list_builder()
+
+print(average_calculator(user_input_words))
